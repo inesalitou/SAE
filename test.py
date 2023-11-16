067dba93d005da858b6a65409d723722dfaa89a4
inesalitou <inesalitou.ra@gmail.com>
2023-11-16 11:07:34 +0100

    Test 2 - nombre négatve - FAILED

--- test.py
+++ test.py
@@ -4,6 +4,23 @@ from script import JeuJustePrix
 
 class TestJeuJustePrix(unittest.TestCase):
 
+    @patch('builtins.input', side_effect=['-5'])
+    def test_jeu_juste_prix_nombre_negatif(self, mock_input):
+        jeu = JeuJustePrix()
+        
+        # Capture la sortie standard pour vérifier le message d'erreur
+        with unittest.mock.patch('sys.stdout', new_callable=unittest.mock.StringIO) as mock_stdout:
+            try:
+                essai = int(input())
+            except ValueError:
+                print("Veuillez entrer un nombre valide.")
+
+        resultat = jeu.deviner(essai)
+        print(resultat)
+
+        self.assertTrue("Veuillez entrer un nombre valide." in mock_stdout.getvalue(), "Le jeu n'a pas indiqué une erreur pour un nombre négatif.")
+        
+
     @patch('builtins.input', side_effect=['50', '75', '88', '90', '92', '91'])
     def test_jeu_juste_prix(self, mock_input):
         jeu = JeuJustePrix()
@@ -12,6 +29,7 @@ class TestJeuJustePrix(unittest.TestCase):
                 essai = int(input())
             except ValueError:
                 print("Veuillez entrer un nombre valide.")
+                # Si l'entrée n'est pas un nombre, on continue la boucle
                 continue
 
             resultat = jeu.deviner(essai)
