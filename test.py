616c55946513b31bd06818a050982d99eed73ca5
inesalitou <inesalitou.ra@gmail.com>
2023-12-13 21:26:07 +0100

    Test 4 - Niveau du jeu - FAILED

--- test.py
+++ test.py
@@ -61,5 +61,37 @@ class TestJeuJustePrix(unittest.TestCase):
                 self.assertTrue("Bravo" in resultat.strip(), "Le jeu n'a pas indiqué la victoire.")
                 break
 
+    @patch('builtins.input', side_effect=['50', '75', '88', '90', '92', '91'])
+    def test_jeu_juste_prix_affichage_niveau(self, mock_input):
+        jeu = JeuJustePrix()
+
+        niveau_attendu = None
+
+        for _ in range(6):
+            try:
+                essai = int(input())
+            except ValueError:
+                print("Veuillez entrer un nombre valide.")
+                continue
+
+            resultat = jeu.deviner(essai)
+            print(resultat)
+
+            if "Veuillez entrer un nombre valide." in resultat:
+                break
+
+            if "Bravo" in resultat:
+                niveau_attendu = jeu.get_niveau()
+                self.assertTrue("Bravo" in resultat.strip(), "Le jeu n'a pas indiqué la victoire.")
+                break
+
+        self.assertIsNotNone(niveau_attendu, "Le niveau n'a pas été attribué.")
+        print(f"Niveau attendu : {niveau_attendu}")
+
+        # Capture la sortie standard pour vérifier si le niveau est affiché
+        with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            print(f"Niveau affiché : {niveau_attendu}")
+            self.assertIn(f"Niveau : {niveau_attendu}", mock_stdout.getvalue(), "Le niveau du joueur n'a pas été affiché.")
+
 if __name__ == '__main__':
     unittest.main()
